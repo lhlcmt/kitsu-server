58acc196e436daff9c6a8744a18d246e8a7698bb
lhlcmt <leahleco@icloud.com>
2022-04-11 10:27:14 +0200

    changed database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
-module.exports = ({ env }) => ({
+/*module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
@@ -7,6 +7,19 @@ module.exports = ({ env }) => ({
       database: env('DATABASE_NAME', 'ID338780_kitsu'),
       user: env('DATABASE_USERNAME', 'ID338780_kitsu'),
       password: env('DATABASE_PASSWORD', 'Léah1252002'),
+      ssl: env.bool('DATABASE_SSL', false),
+    },
+  },
+});*/
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'mysql',
+    connection: {
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT', 3306),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
